eeccbf25058ffbf051f18948a81fc425091506d6
Hitanshi-Sharma <103354645+Hitanshi-Sharma@users.noreply.github.com>
2022-07-16 12:38:17 +0530

    Add files via upload

---
+++ Assignment 7 output.py
@@ -0,0 +1,30 @@
+C:\Users\dogra\PycharmProjects\pythonProject\venv\Scripts\python.exe C:/Users/dogra/AppData/Roaming/JetBrains/PyCharmCE2022.1/scratches/scratch_3.py
+Enter size of list: 6
+Enter marks: 34
+Enter marks: 45
+Enter marks: 56
+Enter marks: 78
+Enter marks: 89
+Enter marks: 90
+[34, 45, 56, 78, 89, 90]
+Enter size of array: 5
+Enter element 4
+Enter element 5
+Enter element 6
+Enter element 7
+Enter element 8
+[4, 5, 6, 7, 8]
+Enter element to be searched and counted: 6
+Element is present at index 2
+Number of occurrences of element is 1
+Enter number of inputs:7
+Enter number:4
+Enter number:3
+Enter number:5
+Enter number:2
+Enter number:6
+Enter number:1
+Enter number:9
+[1, 2, 3, 4, 5, 6, 9]
+
+Process finished with exit code 0
